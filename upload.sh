4f502ca3a1ad2f61b4a1ce63fe308a395c345fb8
Little-Captain <coder.littlecaptain@gmail.com>
2020-03-19 19:48:38 +0800

    更新git脚本

--- upload.sh
+++ upload.sh
@@ -3,7 +3,9 @@
 echo -e '\n添加，提交\n'
 git add .
 git commit
-echo -e '\n推送到gitee\n'
-git push gitee master
+echo -e '\n推送到aliyun\n'
+git push aliyun master
 echo -e '\n推送到github\n'
 git push github master
+echo -e '\n推送到gitee\n'
+git push gitee master
